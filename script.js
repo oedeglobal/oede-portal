77aea9c11bd5bc38c82af185ec888d55e1593094
oedeglobal <yesdguedu@gmail.com>
2026-06-06 17:25:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.querySelectorAll('a[href^="#"]').forEach(a=>{a.addEventListener('click',e=>{const id=a.getAttribute('href');const el=document.querySelector(id);if(el){e.preventDefault();el.scrollIntoView({behavior:'smooth'});}})});
+document.addEventListener('DOMContentLoaded',()=>{document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const t=document.querySelector(a.getAttribute('href'));if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth'});}}));});
